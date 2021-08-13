dfedcfc72358b345852cc39d46da1c6dbe117ba5
Alice-63 <alicetindag84@gmail.com>
2021-08-13 09:30:17 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,35 +1,60 @@
-Vue.component("test3",{
-    template:"<button class='btn btn-info' @click='zahl++'> Klick :{{zahl}}</button>",
-    data:function(){
-        return{zahl:0}
-    }
-
-})
+Vue.component("test",{template:"<button> Ali</button>"})
 
 
-const app=new Vue
-({
-    el:"#app",
-  components:
-  {
-      test:
-      {
-          template:"<button class='btn btn-primary' > Klick </button>",
-          data:function() {
-              return {zahl:0}
-          }
-      },
-      test2:
-      {
-          template:"<button class='btn btn-warning' > Klick:{{zahl}}</button>"
-          ,data:function() {
-            return {zahl:0}
-        }
-        },
+
+// const app= new Vue({
+//     el:"#app",
+//     data:
+//     {
+
+//     },
+//     components:
+//     {
+//         test1:
+//         {
+//             template:"<button>Test2</button>"
+//         }
+//     }
+// })
+
+
+
+
+
+
+
+// Vue.component("test3",{
+//     template:"<button class='btn btn-info' @click='zahl++'> Klick :{{zahl}}</button>",
+//     data:function(){
+//         return{zahl:0}
+//     }
+
+// })
+
+
+// const app=new Vue
+// ({
+//     el:"#app",
+//   components:
+//   {
+//       test:
+//       {
+//           template:"<button class='btn btn-primary' > Klick </button>",
+//           data:function() {
+//               return {zahl:0}
+//           }
+//       },
+//       test2:
+//       {
+//           template:"<button class='btn btn-warning' > Klick:{{zahl}}</button>"
+//           ,data:function() {
+//             return {zahl:0}
+//         }
+//         },
     
-  },
+//   },
 
-})
+// })
 
 
 
