690a37ff0980fd0b67af53006903064f2c4a104c
Alice-63 <alicetindag84@gmail.com>
2021-08-14 09:47:53 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,22 +1,65 @@
 
-Vue.component("hallo",
-{
-    template:"<button> Click </button>"
-})
 
-const app=new Vue({
+Vue.component("hallo",{template:"<button> Click</button"})
+
+
+
+const app= new Vue({
     el:"#app",
-    data:{
+    data:
+    {
 
     },
     components:
     {
         hallo1:
         {
-            template:"<button style='color:blue'> Click </button>"
+            template:"<button> Click Me</button"
         }
     }
+
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Vue.component("hallo",
+// {
+//     template:"<button> Click </button>"
+// })
+
+// const app=new Vue({
+//     el:"#app",
+//     data:{
+
+//     },
+//     components:
+//     {
+//         hallo1:
+//         {
+//             template:"<button style='color:blue'> Click </button>"
+//         }
+//     }
+// })
 
 
 
