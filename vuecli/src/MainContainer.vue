cc88b2dfba47e936df06a5f1e5375ea45270078c
Alice-63 <alicetindag84@gmail.com>
2021-08-15 06:56:55 +0200

    add

---
+++ vuecli/src/MainContainer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <span>{{text}}</span>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae, 
+           equi reiciendis a ad aspernatur voluptate explicabo temporibus totam.
+            Provident, minima neque! Nesciunt quae deserunt debitis iusto natus est praesentium.</p>
+    </div>
+</template>
+<script>
+
+
+export default
+{
+    data(){
+        return
+        {
+            text:"Lorem ipsum dolor sit amet consectetur"
+
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
