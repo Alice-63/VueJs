d6136fd16c81129c069b3cbb8e7730f8bdbff62d
Alice-63 <alicetindag84@gmail.com>
2021-08-19 21:33:43 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,5 +1,25 @@
 
 
+const app= new Vue
+(
+    {
+        el:"#app2",
+        data:
+        {
+            name:"Ali",
+            surname:"Cetindag",
+            age:37
+        },
+        computed:
+        {
+            fullname:function()
+            {
+                return this.name+ " "+ this.surname+" "+this.age
+            }
+        }
+    }
+)
+
 
 
 
