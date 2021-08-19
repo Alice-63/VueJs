cd8ade519e8e746ec990835c3e9d7ac2792cba53
Alice-63 <alicetindag84@gmail.com>
2021-08-19 19:17:10 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,39 +1,5 @@
 
 
-const app= new Vue({
-
-
-    el:"#app",
-    data:
-    {
-        password:""
-    },
-    methods:
-    {
-        check: function()
-        {
-           if(this.password.match(/[a-z]/))
-           {
-                console.log("Kücük Harf")
-           }
-           if(this.password.match(/[A-Z]/))
-           {
-            console.log("Büyük Harf")
-           }
-
-           if(this.password.match(/[0-9]/))
-           {
-            console.log("Sayi")
-           }
-           if(this.password.match(/[a-z]/))
-           {
-
-           }
-        }
-    }
-})
-
-
 
 
 
