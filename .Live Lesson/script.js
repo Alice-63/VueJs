a950a3330cd03aa45c42668744adab9b59dcbda6
Alice-63 <alicetindag84@gmail.com>
2021-08-20 07:35:12 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,48 +1,48 @@
-const daten=
-[
-   {
-       titel:"Ali",
-       text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
-       link:"Link",
-       bild:"1.png"
-   },
-   {
-    titel:"Mustafa",
-    text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
-    link:"Link 2",
-    bild:"2.png"
-   },
-   {
-    titel:"Zeynep",
-    text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
-    link:"Link 3",
-    bild:"3.png"
-   },
-   {
-    titel:"Ali",
-    text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
-    link:"Link",
-    bild:"1.png"
-},
-{
- titel:"Mustafa",
- text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
- link:"Link 2",
- bild:"2.png"
-},
-{
- titel:"Zeynep",
- text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
- link:"Link 3",
- bild:"3.png"
-},
-{
-    titel:"Zeynep",
-    text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
-    link:"Link 3",
-    bild:"3.png"
-   }
-]
+// const daten=
+// [
+//    {
+//        titel:"Ali",
+//        text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
+//        link:"Link",
+//        bild:"1.png"
+//    },
+//    {
+//     titel:"Mustafa",
+//     text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
+//     link:"Link 2",
+//     bild:"2.png"
+//    },
+//    {
+//     titel:"Zeynep",
+//     text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
+//     link:"Link 3",
+//     bild:"3.png"
+//    },
+//    {
+//     titel:"Ali",
+//     text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
+//     link:"Link",
+//     bild:"1.png"
+// },
+// {
+//  titel:"Mustafa",
+//  text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
+//  link:"Link 2",
+//  bild:"2.png"
+// },
+// {
+//  titel:"Zeynep",
+//  text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
+//  link:"Link 3",
+//  bild:"3.png"
+// },
+// {
+//     titel:"Zeynep",
+//     text:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, voluptatum",
+//     link:"Link 3",
+//     bild:"3.png"
+//    }
+// ]
 
 
 // const app= new Vue({
