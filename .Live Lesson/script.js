04fc88616d4080d3bae7d6b270e90664499e94cc
Alice-63 <alicetindag84@gmail.com>
2021-08-22 13:48:40 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,25 +1,100 @@
+const teilnehmer =
+[
+    {
+        id: 0,
+        ad: "Max",
+        soyad: "Mustermann",
+        puan: 84,
+        bolum: 2
+    },
+    {
+        id: 1,
+        ad: "Martin",
+        soyad: "Müller",
+        puan: 100,
+        bolum: 0
+    },
+    {
+        id: 2,
+        ad: "Maria",
+        soyad: "Musterfrau",
+        puan: 52,
+        bolum: 2
+    },
+    {
+        id: 3,
+        ad: "Max",
+        soyad: "Nachname",
+        puan: 93,
+        bolum: 1
+    },
+    {
+        id: 4,
+        ad: "Anna",
+        soyad: "Bella",
+        puan: 70,
+        bolum: 1
+    },
+    {
+        id: 5,
+        ad: "Johannes",
+        soyad: "Kempler",
+        puan: 15,
+        bolum: 0
+    },
+    {
+        id: 6,
+        ad: "Bastian",
+        soyad: "Alt",
+        puan: 98,
+        bolum: 2
+    },
+]
 
-const app=  new Vue({
-    el:"#app",
-    data:
+const bolumler =
+[
     {
-        sayi:13,
-        sayi2:23,
-        sayi3:32
+        id: 0,
+        isim: "Matematik"
     },
-    methods:
     {
-        add:function()
-        {
-           return Math.round(Math.random()*100);
-         
-        }
+        id: 1,
+        isim: "Dil Bilgisi"
     },
-    computed:
     {
-       add2:function()
-       {
-        return Math.round(Math.random()*100)
-       }
+        id: 2,
+        isim: "Spor"
     }
+]
+
+
+const teil=teilnehmer.filter(function(x)
+{
+    
+    return x.puan>=100
 })
+
+function mat(){
+
+    if(bolumler.id==0)
+    {
+        teilnehmer.filter(function(x){
+            return x.puan
+        })
+    }
+}
+
+
+
+const app= new Vue({
+
+el:"#app",
+data:
+    {
+        dat: teil
+    }
+
+
+})
+
+console.log(mat())
