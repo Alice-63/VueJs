3a9a14e2bcc72e936a17fb92847472585c3f1114
Alice-63 <alicetindag84@gmail.com>
2021-08-22 12:43:04 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -1,1 +1,21 @@
 
+const app=  new Vue({
+    el:"#app",
+    data:
+    {
+        sayi:13,
+        sayi2:23,
+        sayi3:32
+    },
+    methods:
+    {
+        add:function()
+        {
+            this.sayi+this.sayi2
+        }
+    },
+    computed:
+    {
+
+    }
+})
