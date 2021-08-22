9065a447944727e8d69ef170f4adc07f339bfa6d
Alice-63 <alicetindag84@gmail.com>
2021-08-22 06:31:03 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -11,3 +11,30 @@ const app= new Vue({
     el:"#app",
     data:{}
 })
+
+
+const arr=
+[
+    {
+        name:"Ali",
+        surname:"Cetin",
+        age:37
+    },
+    {
+        name:"Meryem",
+        surname:"Cetin",
+        age:37
+    },
+    {
+        name:"Mustafa",
+        surname:"Cetin",
+        age:12
+    }
+]
+
+const filterr=arr.filter(v)
+{
+    return v.age>=18
+}
+
+console.log(filterr)
