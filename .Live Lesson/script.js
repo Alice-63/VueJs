7099a3cb76f7f8ea86fad030cba9a941db831f00
Alice-63 <alicetindag84@gmail.com>
2021-08-24 11:06:48 +0200

    add

--- .Live Lesson/script.js
+++ .Live Lesson/script.js
@@ -6,7 +6,8 @@ new Vue({
         changeTitel:"Ali",
         message:"Ali Cetindag",
         link:"http://www.google.com",
-        box:"background-color:red"
+        box:"background-color:red",
+        blogLink:"<a href='https://www.youtube.com/'>Link</a>"
     },
     methods:
     {
