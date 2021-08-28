8118458a5594809a96196f27c7031bb13b1fdcdc
Alice-63 <alicetindag84@gmail.com>
2021-08-28 11:56:48 +0200

    add

---
+++ newprojekt1/src/components/KayitFormu.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Kayit Formu</h1>
+    </div>
+</template>
--- newprojekt1/src/views/Home.vue
+++ newprojekt1/src/views/Home.vue
@@ -2,17 +2,23 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <KayitFormu/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import KayitFormu from "../components/KayitFormu.vue"
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  components:
+  {
+    KayitFormu
   }
 }
 
