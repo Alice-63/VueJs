f487ef7ff24bf8384d16af6c39a7f6ec70ccaced
Alice-63 <alicetindag84@gmail.com>
2021-08-30 23:32:59 +0200

    add

--- bolum-4-canavar-avi-oyunu-template/app.js
+++ bolum-4-canavar-avi-oyunu-template/app.js
@@ -5,7 +5,8 @@ new Vue({
     {
         player_heal:100,
         monster_heal:100,
-        game_is_on:false
+        game_is_on:false,
+        logs:[]
     },
     methods:
     {
@@ -15,6 +16,7 @@ new Vue({
         attack:function(){
             var point=Math.ceil(Math.random()*10);
             this.monster_heal-=point;
+            this.add_to_log({turn:"p", text: "Oyuncu Atagi ("+point+")"})
             this.monster_attack()
            
 
@@ -22,6 +24,7 @@ new Vue({
         special_attack:function(){
             var point=Math.ceil(Math.random()*20);
             this.monster_heal-=point;
+            this.add_to_log({turn:"p", text: "Özel Oyuncu Atagi ("+point+")"})
             this.monster_attack()
             
 
@@ -31,6 +34,7 @@ new Vue({
 
             var point=Math.ceil(Math.random()*20);
             this.player_heal+=point;
+            this.add_to_log({turn:"p", text: "Ilk Yardim ("+point+")"})
             this.monster_attack()
            
 
@@ -42,9 +46,12 @@ new Vue({
 
             var point=Math.ceil(Math.random()*15);
             this.player_heal-=point;
+            this.add_to_log({turn:"p", text: "Canavar Atagi ("+point+")"})
         
-
-           
+        },
+        add_to_log:function(log){
+            this.logs.push(log)
+            console.log(this.logs)
         }
     },
     watch:
@@ -54,6 +61,11 @@ new Vue({
             if(value<=0)
             {
                 this.player_heal=0;
+                if(confirm("Oyunu KAYBETTINIZ. Tekrar etmek ister misniz?"))
+                {
+                    this.player_heal=100,
+                    this.monster_heal=100
+                }
             }
             else if(value>=100)
             {
@@ -64,6 +76,11 @@ new Vue({
             if(value<=0)
             {
                 this.monster_heal=0
+                if(confirm("Oyunu KAZANDINIZ. Tekrar etmek ister misniz?"))
+                {
+                    this.player_heal=100,
+                    this.monster_heal=100
+                }
             }
         }
     }
