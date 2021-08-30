34112d51bff28e77ef49b61fea3a4314d4d9be3d
Alice-63 <alicetindag84@gmail.com>
2021-08-30 15:01:59 +0200

    add

---
+++ bolum-4-canavar-avi-oyunu-template/app.js
@@ -0,0 +1,28 @@
+new Vue({
+
+    el:"#app",
+    data:
+    {
+        player_heal:100,
+        monster_heal:100,
+        game_is_on:false
+    },
+    methods:
+    {
+        start_game:function(){
+
+        },
+        attack:function(){
+
+        },
+        special_attack:function(){
+
+        },
+        heal_up:function(){
+
+        },
+        give_up:function(){
+
+        }
+    }
+})
