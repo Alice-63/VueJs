c527f05a3efdaf6320b7842032b7c05b5378e630
Alice-63 <alicetindag84@gmail.com>
2021-08-30 22:30:49 +0200

    Update app.js

--- bolum-4-canavar-avi-oyunu-template/app.js
+++ bolum-4-canavar-avi-oyunu-template/app.js
@@ -17,22 +17,38 @@ new Vue({
             this.monster_heal-=point;
             this.monster_attack()
             console.log(this.monster_heal)
+            console.log(this.player_heal)
 
         },
         special_attack:function(){
+            var point=Math.ceil(Math.random()*20);
+            this.monster_heal-=point;
+            this.monster_attack()
+            console.log(this.monster_heal)
+            console.log(this.player_heal)
 
+            
         },
         heal_up:function(){
 
+            var point=Math.ceil(Math.random()*20);
+            this.monster_heal+=point;
+            this.monster_attack()
+            console.log(this.monster_heal)
+            console.log(this.player_heal)
+
         },
         give_up:function(){
-
+            this.player_heal=0
         },
         monster_attack:function(){
 
-            var point=Math.ceil(Math.random()*10);
+            var point=Math.ceil(Math.random()*15);
             this.player_heal-=point;
+            console.log(this.monster_heal)
             console.log(this.player_heal)
+
+           
         }
     }
 })
