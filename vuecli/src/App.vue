15e78746181db5f62e9d946d5dc3994f05180987
Alice-63 <alicetindag84@gmail.com>
2021-08-31 20:37:41 +0200

    add

--- vuecli/src/App.vue
+++ vuecli/src/App.vue
@@ -1,21 +1,52 @@
 <template>
   <div id="app">
- 
+ <div class="inputs">
+   <input type="text" v-model="inp_name" placeholder="Name">
+   <input type="text" v-model="inp_surname" placeholder="Surname">
+   <input type="number" v-model="inp_age" placeholder="Age">
+   <button>Add</button>
+ </div>
+ <div class="resulst">
+   <div v-for="person in personen" :key="person.id">
+     <div>{{person.name}}</div>
+     <div>{{person.surname}}</div>
+     <div>{{person.age}}</div>
+     <div class="delete">
+       Delete
+     </div>
+   </div>
+ </div>
 
-    <Uhr/>
-     <Uhr/>
+
     
   </div>
   <router-view/>
 </template>
 <script>
 
-import Uhr from "./components/Uhr.vue"
+
 
 export default{
   name:"app",
-  components:{
-    Uhr
+  data:function()
+  {
+    return{
+      personen:
+      [
+        {
+          id:0,
+          name:"Max",
+          surname:"Mustermann",
+          age:25
+        },
+        {
+          id:1,
+          name:"Test",
+          surname:"Mustermann",
+          age:25
+        }
+      ]
+    }
   }
 }
 
@@ -24,12 +55,46 @@ export default{
 
 
 <style scoped>
-div{
-  color: red;
-}
-img
+
+.inputs
 {
-  width: 20%;
+  margin-bottom: 40px;
+  width: 100%;
+  display: flex;
+
+}
+.inputs>input
+{
+  width: 120px;
+  padding:5px 8px;
+  margin-right: 3px;
+  
+}
+.inputs>input:last-of-type
+{
+  width: 50px;
+}
+.delete
+{
+  margin-top: 15px;
+  font-size: 0.8em;
+  color: brown;
+  cursor: pointer;
+  display: block;
+  text-align: right;
+}
+.delete:hover
+{
+  text-decoration: underline;
+}
+
+
+.resulst>div
+{
+  border: 1px solid black;
+  margin-bottom: 25px;
+  padding: 15px;
+  width: 300px;;
 }
 </style>>
 
--- vuecli/src/views/Home.vue
+++ vuecli/src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-   <p>Lorem ipsum dolor sit amet.</p>
+  
+   
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+
 
 
 
