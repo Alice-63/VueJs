b0967768a9766497fec3156538e24a6b5518e006
Alice-63 <alicetindag84@gmail.com>
2021-09-04 23:07:34 +0200

    add

--- mysocialmedia/newnew/src/App.vue
+++ mysocialmedia/newnew/src/App.vue
@@ -1,47 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-
-  <Test v-for="p in prs" :ad="p.ad" :soyad="p.Soyad" :yas="p.yas" :key="p.id"/>
-
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-
-const person=[
-  {
-    ad:"Ali",
-    Soyad:"Cetin",
-    yas:37
-  },
-  {
-    ad:"Mustafa",
-    Soyad:"Cetin",
-    yas:12
-  },
-  {
-    ad:"Meryem",
-    Soyad:"Cetin",
-    yas:36
-  }
-]
-import Test from "./components/Test.vue"
-
-export default {
-  name: 'App',
-  data:function(){
-    return{
-      prs:person
-    }
-  },
- components:
-{
-  Test
-}
- 
-}
-</script>
 
 <style>
 #app {
@@ -50,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ mysocialmedia/newnew/src/views/Test2.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1> This is a  Test 2 Page</h1>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
