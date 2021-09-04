d7a3db17e18423cee24bc4a8677064a410efb2a1
Alice-63 <alicetindag84@gmail.com>
2021-09-04 13:13:41 +0200

    add

---
+++ mysocialmedia/newnew/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+<input type="text" v-model="namem">
+
+    <Test v-bind:ad=nm key="nm"/>
+    <p>{{namem}}</p>
+  </div>
+</template>
+
+<script>
+import Test from './components/Test.vue'
+
+export default {
+  name: 'App',
+  data:function()
+  { return  {  namem:""}
+  },
+  components: {
+    Test
+  },
+  computed:
+  {
+    nm:this.namem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ mysocialmedia/newnew/src/components/Test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>ALi</h1>
+        <h2>{{ad}}</h2>
+    </div>
+</template>
+<script>
+export default {
+    props:
+    {
+        ad:String
+    }
+}
+</script>
