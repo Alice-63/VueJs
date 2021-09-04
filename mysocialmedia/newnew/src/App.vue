61c32805c14ed6ae0bf5f657e38f2a825738fcf3
Alice-63 <alicetindag84@gmail.com>
2021-09-04 22:34:49 +0200

    Update App.vue

--- mysocialmedia/newnew/src/App.vue
+++ mysocialmedia/newnew/src/App.vue
@@ -2,19 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
 
-
+  <Test/>
 
   </div>
 </template>
 
 <script>
-
+import Test from "./components/Test.vue"
 
 export default {
   name: 'App',
-  data:function()
-  { return  
-  }
+ components:
+{
+  Test
+}
  
 }
 </script>
