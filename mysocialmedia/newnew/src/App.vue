cc4771dd6a6783df7aec30a1249c16c12448bd38
Alice-63 <alicetindag84@gmail.com>
2021-09-04 22:30:26 +0200

    Update App.vue

--- mysocialmedia/newnew/src/App.vue
+++ mysocialmedia/newnew/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-<input type="text" v-model="namem">
 
-    <Test v-bind:ad=nm key="nm"/>
-    <p>{{namem}}</p>
+
+
   </div>
 </template>
 
 <script>
-import Test from './components/Test.vue'
+
 
 export default {
   name: 'App',
   data:function()
-  { return  {  namem:""}
-  },
-  components: {
-    Test
-  },
-  computed:
-  {
-    nm:this.namem
+  { return  
   }
+ 
 }
 </script>
 
