c16ad3da5da5d21d63f8da75df06ae3de8199b4a
Alice-63 <alicetindag84@gmail.com>
2021-09-05 23:42:49 +0200

    Update main.js

--- newprojekt1/src/main.js
+++ newprojekt1/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-export const eventBus=new Vue()
+export const eventBus=new Vue({
+  methods:
+  {
+    yeniYas:function(age)
+    {
+      this.$emit("yas",age)
+    }
+  }
+})
 new Vue({
   el: '#app',
   render: h => h(App)
