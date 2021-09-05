6e6092887fcdb0da1748e0dc4a358415701be45e
Alice-63 <alicetindag84@gmail.com>
2021-09-05 09:53:56 +0200

    add

--- newprojekt1/src/App.vue
+++ newprojekt1/src/App.vue
@@ -1,82 +1,13 @@
 <template>
-  <div id="app">
-    <header style="display:flex; justify-content:space-between; align-items:center">
-      <div class="wrapper" style="width:">
-      <div>
-        <img src="./assets/logo.png" alt="" style="width:100px">
-      </div>
-      <div id="nav">
-        <router-link to="/">Home</router-link> 
-      
-      </div>
-      </div>
-    </header>
-   <main> 
-     <input type="text" v-model="inp">
-  <Props :vorname="inp" />
-  
-     
-     <router-view/>
-   </main>
-   <footer>
-     Digitalle Kariere
-     
-   </footer>
-  </div>
+    <app-user></app-user>
 </template>
+
 <script>
-// @ is an alias to /src
-
-import Props from "./components/Props.vue"
-
+  import User from "./components/User";
 
 export default {
-  name: 'Home',
-  components: {
-    Props
-   
-  },
-  props:
-  {
-    
-  },
-  data:function()
-  { return{
-  
-
- 
-    }
-    
-  },
-  computed:
-  {
-    vorname:this.name
+  components : {
+    appUser : User
   }
-  }
-
-
-
-
+}
 </script>
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
